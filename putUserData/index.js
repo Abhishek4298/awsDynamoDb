7aa0395e694b74d53b2246d4547049dbf2cab36a
Abhishek168 <abhibhavsar1998@gmail.com>
2021-07-21 12:23:23 +0530

    aws-sdk=> DynamoDB get and put operation

---
+++ putUserData/index.js
@@ -0,0 +1,25 @@
+'use strict'
+const AWS = require('aws-sdk');
+
+AWS.config.update({ region: "ap-south-1"});
+
+exports.handler = async (event, context) => {
+  const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08"});
+  const documentClient = new AWS.DynamoDB.DocumentClient({ region: "ap-south-1"});
+
+  const params = {
+    TableName: "Users",
+    Item: {
+      id: "2",
+      firstname: "karan",
+      lastname: "patel"
+    }
+  }
+
+  try {
+    const data = await documentClient.put(params).promise();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
